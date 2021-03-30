b32101bbc6463cb4ba5a4c7e6e45dceea9208afc
Sargis Mkoyan <s.mkoyan18@gmail.com>
2021-03-30 13:21:25 +0400

    database connection options cleanup

--- sequelize/index.js
+++ sequelize/index.js
@@ -8,8 +8,11 @@ const { Sequelize } = require("sequelize");
 console.log(process.env.DATABASE_URL)
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
-        ssl: true,
-    }
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
 });
 
 module.exports = sequelize;
